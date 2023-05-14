316aabb67bb7d24398d1e2ef841b21b940dd8e56
EugeneChen <yujiechen0514@gmail.com>
2023-05-14 21:19:06 +0800

    git commit -m "install & initialize sequelize"

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
+const db = require('./models')
+const Todo = db.Todo;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/todos', (req, res, next) => {
+    Todo.findAll({})
+        .then((todos) => {
+            res.send({todos})
+        })
+        .catch(next)
 })
 
 app.listen(3000, () => {
