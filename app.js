ace810ab823fd8a40aadbc6e8c6265c8763252f5
EugeneChen <yujiechen0514@gmail.com>
2023-05-15 15:48:21 +0000

    create

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -23,6 +25,18 @@ app.get('/todos', (req, res, next) => {
         })
         .catch(next)
 })
+
+app.get('/todos/new', (req, res, next) => {
+    return res.render('new')
+})
+
+app.post('/todos', (req, res, next) => {
+    const name = req.body.name
+    console.log(name)
+    return Todo.create({name})
+        .then(() => res.redirect('/todos'))
+})
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
