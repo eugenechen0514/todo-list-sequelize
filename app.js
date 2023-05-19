c2adb893740deb2f15a098aa4f9295daede6219d
EugeneChen <yujiechen0514@gmail.com>
2023-05-19 12:45:52 +0000

    add flash messages

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const db = require('./models')
 const Todo = db.Todo;
@@ -12,6 +14,13 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(session({
+	secret: 'ThisIsSecret',
+	resave: false,
+	saveUninitialized: false
+}))
+app.use(flash())
+
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -23,7 +32,7 @@ app.get('/todos', (req, res, next) => {
     })
         .then((todos) => {
             console.log(todos)
-            res.render('todos', {todos})
+            res.render('todos', {todos, message: req.flash('success')})
         })
         .catch(next)
 })
@@ -36,7 +45,10 @@ app.post('/todos', (req, res, next) => {
     const name = req.body.name
     console.log(name)
     return Todo.create({name})
-        .then(() => res.redirect('/todos'))
+        .then(() => {
+            req.flash('success', '新增成功!')
+            return res.redirect('/todos')
+        })
 })
 
 app.get('/todos/:id', (req, res, next) => {
@@ -59,25 +71,24 @@ app.get('/todos/:id/edit', (req, res) => {
 
 app.put('/todos/:id', (req, res) => {
     const body = req.body
+    console.log(req.body)
     const {name, isComplete} = body
     const id = req.params.id
     console.log(req.body)
     return Todo.update({name, isComplete: isComplete === 'completed'}, {where: {id}})
-            .then((todo) => res.redirect('/todos'))
-    // return Todo.findByPk(id, {
-    //     attributes: ['id', 'name'],
-    // })
-    //     .then((todo) => {
-    //         todo.name = req.body.name
-    //         return todo.save()
-    //     })
-    //     .then((todo) => res.redirect('/todos'))
+            .then((todo) => {
+                req.flash('success', '俢改成功!')
+                return res.redirect('/todos')
+            })
 })
 
 app.delete('/todos/:id', (req, res) => {
     const id = req.params.id
     return Todo.destroy({where: {id}})
-        .then(() => res.redirect('/todos'))
+        .then(() => {
+            req.flash('success', '刪除成功!')
+            return res.redirect('/todos')
+        })
 })
 
 app.listen(3000, () => {
